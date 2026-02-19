0f8a7f86bba02801aa4f671e765cc3964a5a5517
Jyoti <jyotidhamija2709@gmail.com>
2026-02-19 09:56:58 +0530

    fix

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 /* API Configuration */
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = `${window.location.origin}/api`;
 
 /* Toast Notification Function */
 function showToast(message, type = 'success') {
